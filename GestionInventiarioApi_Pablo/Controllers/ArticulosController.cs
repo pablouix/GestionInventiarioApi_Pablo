5915fe6a2c602f1c8fbd7604a44eedd63d2fe63d
pablouix <pablouix@gmail.com>
2022-10-05 00:12:59 -0400

    Creando models

---
+++ GestionInventiarioApi_Pablo/Controllers/ArticulosController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestionInventiarioApi_Pablo.Controllers
+{
+    //direccion/articulos/Listar
+    [ApiController]
+    [Route("articulos")]
+    public class ArticulosController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Listar")]
+        public dynamic ListarArticulos()
+        {
+
+
+           // return new { id = 1, descripcion = "Celular"};
+
+
+
+        }
+
+
+        /*
+        [HttpPost]
+        [Route("Guardar")]
+        public dynamic ListarArticulos()
+        {
+
+        }
+        */
+
+    }
+}
---
+++ GestionInventiarioApi_Pablo/Models/Articulos.cs
@@ -0,0 +1,10 @@
+﻿namespace GestionInventiarioApi_Pablo.Models
+{
+    public class Articulos
+    {
+        public int articuloId { get; set; }
+        public string descripcion { get; set; }
+        public string marca { get; set; }
+        public double existencia { get; set; }
+    }
+}
